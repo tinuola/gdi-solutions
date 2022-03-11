b81091c83db106fc730a7ba8e23b823ad0c5d3a8
tinuola <24995224+tinuola@users.noreply.github.com>
2022-03-10 22:47:00 -0800

    Adds folder for JS1 202202

---
+++ JS1_2022-02/display-boxes/script.js
@@ -0,0 +1,56 @@
+// console.log('test')
+
+// Part One
+// Variables
+let numOfBoxes = document.getElementById('number-of-boxes')
+
+let submitBtn = document.getElementById('submit-btn')
+
+let container = document.getElementById('container')
+
+let displayMsg = document.getElementById('display-msg')
+
+
+// Part Two
+// Helper functions used inside the Event Listener
+const addBox = () => {  
+  let newDiv = document.createElement('div')
+  newDiv.className = 'box'
+  container.appendChild(newDiv)
+}
+
+const displayErrorMsg = (msg) => {
+  displayMsg.style.display = 'block';
+  displayMsg.innerHTML = msg;
+}
+
+const clearErrorMsg = () => {
+  displayMsg.style.display = 'none';
+  displayMsg.innerHTML = '';
+}
+
+
+// Part Three
+// Event Listener
+submitBtn.addEventListener('click', function(){
+
+  let num = numOfBoxes.value;
+
+  if( num <= 0 ){
+
+    let error = 'Please enter a positive number'
+
+    displayErrorMsg(error);
+
+  } else {
+
+    for(let i = 1; i <= num; i++){
+
+      addBox();
+
+    }
+
+    clearErrorMsg()
+  }
+
+})
